582a4bf445717eaa5d5407b9c3d125d0980258e3
b1017173 <g2121034@fun.ac.jp>
2021-07-08 00:52:51 +0900

    新規投稿ページの側だけ実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,14 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def new
+  end
+
   def show
     @post = Post.find_by(id: params[:id])
+  end
+
+  def create
+    redirect_to('/posts/index')
   end
 end
