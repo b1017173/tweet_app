1bd2380a335668a793da58610588bdfab2a715ef
b1017173 <g2121034@fun.ac.jp>
2021-07-08 22:53:52 +0900

    投稿の編集・削除機能追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,13 +6,30 @@ class PostsController < ApplicationController
   def new
   end
 
+  def create
+    @post = Post.new(content: params[:content])
+    @post.save
+    redirect_to('/posts/index')
+  end
+
   def show
     @post = Post.find_by(id: params[:id])
   end
 
-  def create
-    @post = Post.new(content: params[:content])
+  def edit
+    @post = Post.find_by(id: params[:id])
+  end
+
+  def update
+    @post = Post.find_by(id: params[:id])
+    @post.content = params[:content]
     @post.save
+    redirect_to('/posts/index')
+  end
+
+  def destroy
+    @post = Post.find_by(id: params[:id])
+    @post.destroy
     redirect_to('/posts/index')
   end
 end
