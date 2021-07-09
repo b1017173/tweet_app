eafe6500d7e9bc1e8a0d960685d5856bd33a29d1
b1017173 <g2121034@fun.ac.jp>
2021-07-10 03:19:06 +0900

    ユーザ情報の変更の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,22 @@ class UsersController < ApplicationController
     @user = User.find_by(id: params[:id])
   end
 
+  def edit
+    @user = User.find_by(id: params[:id])
+  end
+
+  def update
+    @user = User.find_by(id: params[:id])
+    @user.name = params[:name]
+    @user.email = params[:email]
+    if @user.save
+      flash[:notice] = "ユーザー情報を編集しました"
+      redirect_to("/users/#{@user.id}")
+    else
+      render("users/edit")
+    end
+  end
+
   def new
     @user = User.new
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get 'users/index' => 'users#index'
   post 'users/create' => 'users#create'
   get 'users/:id' => 'users#show'
+  get 'users/:id/edit' => 'users#edit'
+  post 'users/:id/update' => 'users#update'
 
   get 'posts/index' => 'posts#index'
   get 'posts/new' => 'posts#new'
