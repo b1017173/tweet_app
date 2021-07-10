e7cc2729c0bd512453a6fe8e6d16e53b9d8ef2cb
b1017173 <g2121034@fun.ac.jp>
2021-07-11 03:59:00 +0900

    ユーザのログイン機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -16,5 +16,7 @@ Rails.application.routes.draw do
   get '/' => 'home#top'
   get 'about' => 'home#about'
   get 'signup' => 'users#new'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'login' => 'users#login_form'
+  post 'login' => 'users#login'
+  post 'logout' => 'users#logout'
 end
